1d54596aee8a35b81ba67246b28aab55bad80691
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-23 15:34:25 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide=() => {
+    return 2000/100;
+} 
+divide();
+const square=(x) => {
+    return x**2;
+}
+square(2);
+const add=() => {
+    let a=3
+    let b=4
+    return a+b;
+}
+add();
